db05fe0f44ce2df4c77ea3081ae5c6fd7a8b0ff4
William Liang <williamliang0902@gmail.com>
2026-06-19 13:26:29 -0400

    Create the FullTimeEmployee and PartTimeEmployee subclasses, derived from the Employee class

--- Assignment2_EmployeeManagement/EmployeeFile.swift
+++ Assignment2_EmployeeManagement/EmployeeFile.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 
+///Represents an employee in the employee management system
 class Employee
 {
     var id: Int
@@ -16,7 +17,7 @@ class Employee
         return 0
     }
     
-    init(id: Int, name: String, salary: Double) {
+    init(id: Int, name: String) {
         self.id = id
         self.name = name
     }
@@ -26,3 +27,43 @@ class Employee
         return "ID: \(id) | Name: \(name) | Salary: \(salary)"
     }
 }
+
+class FullTimEmployee : Employee
+{
+    var monthlySalary: Double
+    
+    init(id: Int, name: String, monthlySalary: Double)
+    {
+        self.monthlySalary = monthlySalary
+        super.init(id: id, name: name)
+    }
+    
+    override var salary: Double
+    {
+        let annualSalary = 12 * monthlySalary
+        return annualSalary
+    }
+}
+
+class PartTimeEmployee : Employee
+{
+    var hourlyRate: Double
+    var hoursWorked: Int
+    
+    init(id: Int, name: String, hourlyRate: Double, hoursWorked: Int) {
+        self.hourlyRate = hourlyRate
+        self.hoursWorked = hoursWorked
+        super.init(id: id, name: name)
+    }
+    
+    override var salary: Double
+    {
+        let annualSalary = hourlyRate * Double(hoursWorked) * 56
+        return annualSalary
+    }
+}
+
+
+
+
+
