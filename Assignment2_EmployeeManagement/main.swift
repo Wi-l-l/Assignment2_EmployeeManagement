03f35ac30a97bb52797667940f18f068c6fa460d
William Liang <williamliang0902@gmail.com>
2026-06-19 13:43:16 -0400

    - Create the dictionary to store multiple employees - create the main menu of the application

--- Assignment2_EmployeeManagement/main.swift
+++ Assignment2_EmployeeManagement/main.swift
@@ -6,6 +6,18 @@
 //
 
 import Foundation
+var employees: [Int:Employee] = [:]
+menu()
 
-
-
+func menu()
+{
+    print("\n=== Employee Management System ==="
+        + "\nPlease select one of the following options"
+        + "\n1. Add a Full-Time Employee"
+        + "\n2. Add a Part-Time Employee"
+        + "\n3. Display All Employees"
+        + "\n4. Remove an employee by ID"
+        + "\n5. Search for an employee by ID"
+        + "\n6. Exit"
+        + "\n\nEnter your option:")
+}
