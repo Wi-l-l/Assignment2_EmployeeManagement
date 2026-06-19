94a6741f4a8c0512b287db6f23e2c8754af73575
William Liang <williamliang0902@gmail.com>
2026-06-19 14:45:58 -0400

    Implement options to add both full-time and part-time employees to the employees dictionary

--- Assignment2_EmployeeManagement/main.swift
+++ Assignment2_EmployeeManagement/main.swift
@@ -18,6 +18,7 @@ while toContinue
     switch input
     {
     case "1":
+        print("\nAdding a Full-Time Employee...")
         addFullTimeEmployee()
     case "2":
         addPartTimeEmployee()
@@ -49,18 +50,53 @@ func menu()
         + "\n\nEnter your option:")
 }
 
-//TODO: Add a full-time employee
+//Add a full-time employee
 //TODO: Implement add full-time employee validation
 func addFullTimeEmployee()
 {
-    //
+    var newFullTimeEmployee: Employee
+    
+    print("\nEnter Employee ID:")
+    var ftEmpID = Int(readLine()!)!
+    
+    print("\nEnter Employee Name:")
+    var ftEmpName = readLine()!
+    
+    print("\nEnter Employee Monthly Salary:")
+    var ftEmpSalary = Double(readLine()!)!
+    
+    newFullTimeEmployee = FullTimEmployee(id: ftEmpID, name: ftEmpName, monthlySalary: ftEmpSalary)
+    employees[ftEmpID] = newFullTimeEmployee
+    
+    //for (id, employee) in employees{
+    //    print(employee.displayDetails())
+    //}
 }
 
 //TODO: Add a part-time employee
 //TODO: Implement add part-time employee validation
 func addPartTimeEmployee()
 {
-    //
+    var newPartTimeEmp: Employee
+    
+    print("\nEnter Employee ID:")
+    var ptEmpID = Int(readLine()!)!
+    
+    print("\nEnter Employee Name:")
+    var ptEmpName = readLine()!
+    
+    print("\nEnter Employee Hourly Rate:")
+    var ptEmpHourlyRate = Double(readLine()!)!
+    
+    print("\nEnter Employee Hours Worked:")
+    var ptEmpWorkHours = Int(readLine()!)!
+    
+    newPartTimeEmp = PartTimeEmployee(id: ptEmpID, name: ptEmpName, hourlyRate: ptEmpHourlyRate, hoursWorked: ptEmpWorkHours)
+    employees[ptEmpID] = newPartTimeEmp
+    
+    //for (id, employee) in employees{
+    //    print(employee.displayDetails())
+    //}
 }
 
 //TODO: Display all employees
@@ -80,3 +116,5 @@ func findEmployee()
 {
     //
 }
+
+
